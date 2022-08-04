1d3b49c89917aa0a9bc85854b3073f0e3bb23a5d
M-java86 <mpythondata@outlook.com>
2022-08-04 15:51:30 -0400

    added item Objects to print out in list

--- src/List.js
+++ src/List.js
@@ -1,5 +1,11 @@
-function List () {
-    return <p>HELLO</p>;
+function List(props) {
+  return (
+    <div>
+      {props.itemList.map(itemObj => {
+       return <p>{itemObj.item}</p>;
+      })}
+    </div>
+  );
 }
 
 export default List;
