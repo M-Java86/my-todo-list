6b08a1a3b6367ac9f63d91f9579331493ad592d7
M-java86 <mpythondata@outlook.com>
2022-08-04 14:44:52 -0400

    added onChangeHandler it output values

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import { useState} from "react";
 
 function App() {
+  const [currentItem, setCurrentItem] = useState(null);
+
+  const onChangehandler = e => {
+    console.log("current value", e.target.value);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <div className="Wrapper">
           <div className="Input-wrapper">
-            <input />
+            <input value={currentItem} onChange={onChangehandler} />
             <button>+</button>
           </div>
         </div>
